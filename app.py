e651c96763efb29880bf4ecb70b2ed47733ea1d5
stepan_varygin <st054552@gsom.spbu.ru>
2023-06-09 23:27:43 +0300

    added core code

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask
+import joblib
+import numpy
+
+MODEL_PATH = 'ML_models/model_Random_Forest_150_9.pkl'
+SCALER_X_PATH = 'ML_models/standard_scaler_x.pkl'
+SCALER_Y_PATH = 'ML_models/standard_scaler_y.pkl'
 
 app = Flask(__name__)
+model = joblib.load(MODEL_PATH)
+sc_x = joblib.load(SCALER_X_PATH)
+sc_y = joblib.load(SCALER_Y_PATH)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/predict_price', methods = ['GET'])
+def predict():
+    args = request.args
+    open_plan = args.get('open_plan', default=-1, type=int)
+    rooms = args.get('rooms', default=-1, type=int)
+    area=args.get('area', default=-1, type=float)
+    renovation = args.get('renovation', default=-1, type=int)
 
+    x = numpy.array([open_plan, rooms, area, renovation]).reshape(1,-1)
+    x = sc_x.transform(x)
+    result = model.predict(x)
+    result = sc_y.inverse_transform(result.reshape(1,-1))
+
+    return str(result[0][0])
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True, port = 5449, host = '0.0.0.0')
